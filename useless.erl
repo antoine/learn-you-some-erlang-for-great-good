58df7a7fddd3c7edd07c89b6289d33fbd705d6e4
antoine <antoine@delaunay.org>
2016-03-17 20:03:58 +0100

    introduction to functions

--- useless.erl
+++ useless.erl
@@ -1,19 +1,11 @@
 -module(useless).
--export([add/2, hello/0, greet_and_add_two/1]).
--compile([debug_info, export_all]).
--define(HOUR, 3600). %in seconds
 
--define(sub(X,Y), X-Y).
+-compile([export_all]).
 
-add(A,B) ->
-  A + B.
+greet(male, Name) ->
+  io:format("Hello Mr. ~s", [Name]);
+greet(female, Name) ->
+  io:format("Hello Mrs. ~s", [Name]);
+greet(_, Name) ->
+  io:format("Hello ~s", [Name]).
 
-sub(A,B) ->
-  ?sub(B,A).
-
-hello() ->
-  io:format("Hello you~n").
-
-greet_and_add_two(X) ->
-  hello(),
-  add(X,2).
